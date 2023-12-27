321e0fb26396fc826574f88a79e758fcbcee7f3c
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-12-27 23:10:58 +0330

    cast

--- app.js
+++ app.js
@@ -1,41 +1,13 @@
-// const assertion (const cast)
-// reasign and mutate
-// let num : 12 = 12 
-var num = 12;
-num = 12;
-var numbers = [12, 13];
-numbers = [12, 13];
-// numbers = [14, 15] 
+// as < >
+// const numbers = <const>[12,13] 
+// const numbers = [12,13] as const
 // numbers.push(20)
-console.log(numbers);
-var user = {
-    id: 1,
-    name: 'Amin'
-};
-console.log(user);
-// user.name = 'ali'
-console.log(user);
-function sum(num1, num2) {
-    return num1 + num2;
-}
-console.log(sum.apply(void 0, numbers));
-var favorites = ['js', "study"];
-var users = [
-    { id: 1, name: 'Amin', favorites: favorites }
-];
-users[0].favorites.push('coding');
-console.log(users);
-// Example 3 
-var actions = [
-    { type: 'ADD_TODO' },
-    { type: "REOMVE_TODO" }
-];
-for (var _i = 0, actions_1 = actions; _i < actions_1.length; _i++) {
-    var action = actions_1[_i];
-    if (action.type === 'CREATED') {
-        console.log('created');
-    }
-    else {
-        console.log('Removed');
-    }
-}
+// Casting in dom project
+var linkElem = document.querySelector('#link');
+var userNameInput = document.querySelector('input');
+var passwordInput = document.querySelector('.password');
+// const btnElem = document.querySelector('button') as HTMLButtonElement
+var btnElem = document.querySelector('button');
+// console.log(btnElem?.innerHTML)
+// console.log(btnElem.innerHTML)
+console.log(btnElem.innerHTML);
--- app.ts
+++ app.ts
@@ -1,8 +1,25 @@
 
+// as < >
+
+// const numbers = <const>[12,13] 
+// const numbers = [12,13] as const
+
+
+// numbers.push(20)
+
+// Casting in dom project
+
+const linkElem = document.querySelector('#link') as HTMLAnchorElement
+const userNameInput = document.querySelector('input') as HTMLInputElement
+const passwordInput = <HTMLInputElement> document.querySelector('.password') 
+// const btnElem = document.querySelector('button') as HTMLButtonElement
+const btnElem = document.querySelector('button')!
 
 
 
-
+// console.log(btnElem?.innerHTML)
+// console.log(btnElem.innerHTML)
+console.log((btnElem as HTMLButtonElement).innerHTML);
 
 
 
