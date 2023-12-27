15912d15b8ef6c411b837ce9ca5d09c0586b7fea
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-12-28 00:52:08 +0330

    void

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-// const ADMIN = 'ADMIN'
-// const USER = 'USER'
-// const TEACHER = 'TEACHER'
-var Role;
-(function (Role) {
-    Role[Role["Admin"] = 0] = "Admin";
-    Role[Role["User"] = 1] = "User";
-    Role[Role["Teacher"] = 2] = "Teacher";
-    Role[Role["Developer"] = 3] = "Developer";
-    Role[Role["Author"] = 4] = "Author";
-})(Role || (Role = {}));
-// fatehe proje ba enum khonde mishe va nabayad estefade kard.
-// az yek enum bishtar nabayad estefedeh konid
+var sum = function (num1, num2) {
+    return num1 + num2;
+};
+var logger = function (param) {
+    console.log('Result => ', param);
+};
+console.log(logger(logger('hiiiiiii')));
+var logger2 = function (param) {
+    console.log('Result => ', param);
+    return undefined;
+};
+console.log(logger('hiiiii'));
--- app.ts
+++ app.ts
@@ -1,23 +1,22 @@
-// const ADMIN = 'ADMIN'
-// const USER = 'USER'
-// const TEACHER = 'TEACHER'
-
-enum Role {
-    Admin,
-    User ,
-    Teacher,
-    Developer,
-    Author
+const sum = (num1 : number , num2 : number ) : number {
+    return num1 + num2
 }
- // fatehe proje ba enum khonde mishe va nabayad estefade kard.
- // az yek enum bishtar nabayad estefedeh konid
+
+const logger = (param):void => {
+    console.log('Result => ', param);
+    
+}
+
+console.log(logger(logger('hiiiiiii')));
 
 
 
+const logger2 = (param):undefined => {
+    console.log('Result => ', param);
+    return undefined
+}
 
-
-
-
+console.log(logger('hiiiii'))
 
 
 
