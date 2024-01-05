60ca0889f5093c020b1c5b7535bf948a06e80848
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2024-01-05 13:42:51 +0330

    lib

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
 "use strict";
-let res = null;
-let res2 = undefined;
-let num = 1;
-const adminLoginHandler = (admin) => {
-    switch (admin.name) {
-        case 'ali': {
-            return `ali login shod`;
-        }
-        case 'amin': {
-            return 'amin login shod';
-        }
-        default: {
-            let neverVariable = admin;
-            return neverVariable;
-        }
-    }
-};
-function errorMarker(msg, code) {
-    throw new Error(JSON.stringify({ message: msg, status: code }));
-}
-errorMarker('Not Found', 404);
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    console.log(btn);
+});
--- app.ts
+++ app.ts
@@ -1,37 +1,10 @@
-let res : null = null ;
-let res2 : undefined = undefined
-let num : 1 | 2 = 1
+const btn = document.querySelector('#btn')!
+
+btn.addEventListener('click',()=>{
+    console.log(btn)
+})
 
 
-// never هرگز
-
-type Ali = {id:1 , name:'ali',age :20}
-type Amin = {id:2 , name:'amin',age :23}
-
-type Admins = Ali | Amin 
-
- const adminLoginHandler = (admin:Admins) => {
-    switch(admin.name) {
-        case 'ali' : {
-            return `ali login shod`
-        }
-        case 'amin' : {
-            return 'amin login shod'
-        }
-        default : {
-            let neverVariable : never = admin
-            return neverVariable
-        }
-    }
- }
-
-
-function errorMarker (msg,code) {
-    throw new Error(JSON.stringify({message : msg , status : code}))
-}
-
-
-errorMarker('Not Found',404)
 
 
 
